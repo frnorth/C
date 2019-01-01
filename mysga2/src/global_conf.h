2c8f2fa6f2ea0e1541f42dbc058aaa87af0af982
Administrator <admin@example.com>
2019-01-01 16:43:06 +0800

    a

---
+++ mysga2/src/global_conf.h
@@ -0,0 +1,10 @@
+
+#ifndef _GLOBAL_CONF_H_INCLUDE_
+#define _GLOBAL_CONF_H_INCLUDE_
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+
+#endif /* _GLOBAL_CONF_H_INCLUDE_ */
