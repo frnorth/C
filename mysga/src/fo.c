4d8e4eda623aba45cc7d5b24b0d78cb41d1f16e0
Administrator <admin@example.com>
2019-01-13 17:22:37 +0800

    fo.c

--- mysga/src/fo.c
+++ mysga/src/fo.c
@@ -10,15 +10,66 @@ double fitness_cal(double * c_variable) {
 	return fitness;
 
 
-//	FILE *fsga; 
-//	fsga = fopen(, "r"); 
+//	char *fo_path = NULL;//argv[1];
+//	if (argc < 2)
+//		fo_path = get_current_dir_name();
+//	int path_length=strlen(fo_path);
+//	int i=0;
 //
+//	char hahac[100];
+//	char list_path[100];
+//	for (i=0;i<path_length-3;i++){
+//		list_path[i]=fo_path[i];
+//	}
+//	list_path[length-3]='/';
+//	list_path[length-2]='l';
+//	list_path[length-1]='i';
+//	list_path[length]='s';
+//	list_path[length+1]='t';
+//
+//	FILE *fo; 
+//	fo = fopen(list_path, "r"); 
+//
+//	int list_length=0;
 //	/* fscanf will scan one raw twice? */
-//	fscanf(fsga, "%8s%d", hahac, &hahad); 
-//	pop_size = hahad; 
-//	printf("%s\t%d\n", hahac, pop_size); 
+//	fscanf(fo, "%5s%d", hahac, &list_length); 
+//	printf("%s\t%d\n", hahac, list_length); 
 //
-//	fscanf(fsga, "%14s%d", hahac, &hahad); 
-//	generation_num = hahad; 
+//	double list_x=(double *)malloc(sizeof(double)*list_length); 
+//	double list_y=(double *)malloc(sizeof(double)*list_length); 
+
+
+//	double a=c_variable[0];
+//
+//	double list_y[100]={1,1,2,3,1,
+//						3,2,8,1,2,
+//						3,2,8,2,1,
+//						7,9,2,1,2};
+//
+//	double fitness=0,avg=0;
+//
+//	int i=0;
+//	for (i=0;i<20;i++){
+//		avg+=list_y[i]/20;
+//		fitness+=pow((list_y[i]-a),2.0);
+//	}
+//
+//	printf("%10lf",avg);
+//
+//	return 1/fitness;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
