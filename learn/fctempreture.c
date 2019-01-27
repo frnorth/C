8772661ee1b31158ef1022ee8c46cf38ba174e9e
Administrator <admin@example.com>
2019-01-27 22:39:02 +0800

    a

--- learn/fctempreture.c
+++ learn/fctempreture.c
@@ -3,7 +3,7 @@
 /* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
 main() {
 
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0 ;		/* lower limit of temperature table */
@@ -11,9 +11,10 @@ main() {
 	step = 20;		/* step size */
 
 	fahr = lower;
+	printf("%5s %9s\n", "fahr", "celsius");
 	while (fahr <= upper){
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%5.0f %9.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 
