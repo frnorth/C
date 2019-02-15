5459e8c59c42b13304e9f339a528fdfe86c93452
Administrator <admin@example.com>
2019-02-15 23:32:29 +0800

    a

--- learn/countchar.c
+++ learn/countchar.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /* count characters in input; 1st version */
-//main() {
-//	long nc;
-//
-//	nc = 0;
-//	while (getchar() != EOF)
-//		++nc;
-//	// 1d还是0d?
-//	printf("%1d\n",nc);
-//}
+main() {
+	long nc;
+
+	nc = 0;
+	while (getchar() != EOF)
+		++nc;
+	// 1d还是0d?,, 是ld, 小写的'L'!
+	printf("%ld\n",nc);
+}
 
 /* count characters in input; 1st version */
-main() {
-	double nc;
-
-	for (nc =0 ; getchar() != EOF; ++nc)\
-		;
-	// .0f还是.1f?
-	printf("%.0f\n",nc);
-}
+//main() {
+//	double nc;
+//
+//	for (nc =0 ; getchar() != EOF; ++nc)\
+//		;
+//	// .0f还是.1f?
+//	printf("%.0f\n",nc);
+//}
