2f00c1942de69bafa4cc52f0a397227e4d7fd6b7
Administrator <admin@example.com>
2019-02-16 23:22:48 +0800

    a

--- learn/countline.c
+++ learn/countline.c
@@ -2,12 +2,13 @@
 
 /* count lines in input */
 main() {
-	int c,n1;
+	int c,nl;
 
-	n1 = 0;
+	nl = 0;
 	// 当输入了一串sfsbgsfsdfs, 按ctrl+d, 这个时候只是停止本次输入, 而不是getchar()一个ctrl+d, 所以getputchar.c运行后, 输入sfsdfsdf, ctrl+d, 不换行, 直接输出了输入的!
 	while ((c = getchar()) != EOF)
 		if (c == '\n')
-			++n1;
-	printf("%d\n", n1);
+			++nl;
+	//putchar(nl); 这个用不了, 因为nl不是getchar进来的?
+	printf("%d\n", nl);
 }
