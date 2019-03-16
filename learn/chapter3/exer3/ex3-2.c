6e9550740430ffc0b1aa8bdb168a0c16eb62a376
wangjie <763211690@qq.com>
2019-03-16 13:30:00 +0800

    ex-3-2

--- learn/chapter3/exer3/ex3-2.c
+++ learn/chapter3/exer3/ex3-2.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 
-#define LENGTH 1000
+#define LENGTH 200
 
 void gettext2(char text[], int maxlength);
 void escape(char s[], char t[]);
+void escape2(char s[], char t[]);
 
 main()
 {
+	int i;
 	char s[LENGTH], t[LENGTH];
 
 	gettext2(t, LENGTH);
-	escape(s, t);
+	//escape(s, t);
+	escape2(s, t);
 
-	printf("%s", s);
+	printf("%s\n", s);
+	for (i = 0; i< LENGTH; i++) {
+		if (s[i] == '\0')
+			printf("100");
+		printf("%c", s[i]);
+	}
+	printf("\n\n");
+
+	printf("%s\n", t);
+	for (i = 0; i< LENGTH; i++) {
+		if (s[i] == '\0')
+			printf("100");
+		printf("%c", t[i]);
+	}
+	printf("\n");
 
 }
 
@@ -44,6 +61,38 @@ void escape(char s[], char t[])
 			break;
 		default:
 			s[j++] = t[i++];
+			break;
 		}
 	}
+	s[i]=t[i];
+}
+
+void escape2(char s[], char t[])
+{
+	int i, j;
+
+	i = j = 0;
+	while(t[i] != '\0') {
+		switch (t[i]) {
+		case '\\':
+			switch (t[i + 1]) {
+			case 't':
+				s[j++]= '\t';
+				i+=2;
+				break;
+			case 'n':
+				s[j++] = '\n';
+				i+=2;
+				break;
+			default:
+				s[j++] = t[i++];
+				break;
+			}
+			break;
+		default:
+			s[j++] = t[i++];
+			break;
+		}
+	}
+	s[i]=t[i];
 }
