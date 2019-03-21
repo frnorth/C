1efa62ed95265908228dffbd7c536bed88423b37
wangjie <763211690@qq.com>
2019-03-21 23:14:46 +0800

    a

--- learn/chapter3/exer3/ex3-3.c
+++ learn/chapter3/exer3/ex3-3.c
@@ -4,6 +4,7 @@
 #define MAX_LENGTH 1000
 
 void gettext2(char text[], int maxlength);
+void expands(char s1[], char s2[]);
 
 main()
 {
@@ -12,6 +13,9 @@ main()
 	char s2[MAX_LENGTH];
 
 	gettext2(s1, LENGTH);
+	expands(s1, s2);
+	printf("%s\n", s1);
+	printf("%s\n", s2);
 
 	return 0;
 }
@@ -27,13 +31,23 @@ void gettext2(char text[], int maxlength)
 
 void expands(char s1[], char s2[])
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0, j = 0; s1[i] != '\0'; i++) {
 		if (s1[i] == '-') {
 			if (i == 0 )
-				;
-			else if ((i - 1) == '\t' && )
+				s2[j++] = s1[i];
+			else if (s1[i - 1] == '\t' || s1[i - 1] == ' ' )
+				s2[j++] = s1[i];
+			else if (s1[i + 1] <= s1[i - 1])
+				s2[j++] = s1[i];
+			else
+				for (k = 1; (s1[i - 1] + k) < s1[i + 1]; k++, j++) {
+					s2[j] = s1[i - 1] + k;
+				}
 		}
+		else
+			s2[j++] = s1[i];
 	}
+	s2[j] = '\0';
 }
