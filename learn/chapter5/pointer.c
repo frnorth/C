f7cb899f2b2cf0bae5af6c30adac9678d10a34e0
wangjie <763211690@qq.com>
2019-04-15 23:08:52 +0800

    a

--- learn/chapter5/pointer.c
+++ learn/chapter5/pointer.c
@@ -2,13 +2,16 @@
 
 #define defprint(x) printf(#x ": %d\n", x)
 
+void swap(int *pa, int *pb);
+
 main()
 {
 	int x = 10, y = 2, z[10];
-	int *ip;
+	int *ip, *iq;
 
-	defprint(ip);
-	defprint(*ip);
+	/* 打开这个注释, 就会报段错误? */
+	//defprint(ip);
+	//defprint(*ip);
 
 	ip = &y;
 	defprint(ip + 1);
@@ -22,5 +25,25 @@ main()
 	defprint(ip + 1);
 	defprint(*(ip + 1));
 
+	iq = ip;
+	defprint(iq);
+	defprint(*iq);
+
+
+	defprint(x);
+	defprint(y);
+	swap(&x, &y);
+	defprint(x);
+	defprint(y);
+
 	return 0;
+}
+
+void swap(int *pa, int *pb)
+{
+	int tmp;
+
+	tmp = *pa;
+	*pa = *pb;
+	*pb = tmp;
 }
