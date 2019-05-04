211945d15c9a10eadbdd7faca864e0cb319c00bd
frnorth <763211690@qq.com>
2019-05-04 20:30:08 +0800

    dcl half

---
+++ learn/chapter5/dcl/getch.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define BUFSIZE 100
+#define EOFSTATE '5'
+
+char buf[BUFSIZE];	/* buffer for ungetch */
+int bufp = 0;		/* next free position in buf */
+
+//int getch(void)		/* get a (possibly pushed back) character */
+//{
+//	return (bufp > 0) ? buf[--bufp] : getchar();
+//}
+
+/* handle the pushed-back EOF */
+int getch(void)
+{
+	if (bufp > 0) {
+		if (buf[--bufp] == EOF)
+			return EOFSTATE;
+		else
+			return buf[bufp];
+	}
+	return getchar();
+}
+
+void ungetch(int c)	/* push character back on input */
+{
+	int i;
+
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+
+	printf("ungetch: ");
+	for (i = 0; i < bufp; i ++)
+		printf("%d %c\t", i, buf[i]);
+	printf("\n");
+}
+
+/* where to use this function? */
+void ungets(char s[])
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		ungetch(s[i++]);
+}
