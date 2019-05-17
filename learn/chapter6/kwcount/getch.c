8493799c5e35d8ec766a0e88dfbfc8728260a545
frnorth <763211690@qq.com>
2019-05-17 14:00:15 +0800

    keyword-count program showed in the book finished learning

---
+++ learn/chapter6/kwcount/getch.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define BUFSIZE 100
+//#define EOFSTATE '5'
+#define EOFSTATE EOF
+
+char buf[BUFSIZE];	/* buffer for ungetch */
+int bufp = 0;		/* next free position in buf */
+
+//int getch(void)		/* get a (possibly pushed back) character */
+//{
+//	return (bufp > 0) ? buf[--bufp] : getchar();
+//}
+
+/* handle the pushed-back EOF */
+int getch(void)
+{	
+	int c;
+
+	//printf("\t--> getch: ");
+	//for (i = 0; i < bufp; i ++)
+	//	printf("%d %c(%d)\t", i, buf[i], buf[i]);
+	//printf("\n");
+
+	if (bufp > 0) {
+		if (buf[--bufp] == EOF) {
+			printf("\t--> getch: %d %c(%d)\n", bufp, buf[bufp], buf[bufp]);
+			return EOFSTATE;
+		}
+		else {
+			printf("\t--> getch: %d %c(%d)\n", bufp, buf[bufp], buf[bufp]);
+			return buf[bufp];
+		}
+	}
+	c = getchar();
+	printf("\t--> getch: %c(%d)\n", c, c);
+	return c;
+}
+
+void ungetch(int c)	/* push character back on input */
+{
+	int i;
+
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+
+	printf("\t--> ungetch: ");
+	for (i = 0; i < bufp; i ++)
+		printf("%d %c(%d)\t", i, buf[i], buf[i]);
+	printf("\n");
+}
+
+/* where to use this function? */
+void ungets(char s[])
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		ungetch(s[i++]);
+}
