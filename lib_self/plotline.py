a2056e218bf7ee9da8545d17ffa9dcb7d25314f3
frnorth <763211690@qq.com>
2021-03-14 14:48:02 +0800

    haha

---
+++ lib_self/plotline.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 不能有空行
+
+
+f=open("/home/wangjie/test/C/xes1/dis/result.txt")
+
+x = []
+y = []
+
+for line in f:
+    dataline = line.split()
+    xx = float(dataline[0]) #split 后, 依然是字符, 所以要转成数字
+    yy = float(dataline[1]) #split 后, 依然是字符, 所以要转成数字
+    x.append(xx)
+    y.append(yy)
+
+f.close()
+
+x = np.array(x)
+y = np.array(y)
+
+fig, ax0 = plt.subplots(figsize=(12, 4))
+ax0.plot(x,y)
+
+ax0.set_title('test')
+
+plt.xlabel("x")
+plt.ylim(0)
+plt.show()
+
