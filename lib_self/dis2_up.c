d9c3aa94c2b78a2023de1f2f2e60303d52364e8e
frnorth <763211690@qq.com>
2021-03-14 14:44:41 +0800

    distribution

---
+++ lib_self/dis2_up.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<time.h>
+
+
+#define N 1000
+#define M 12
+
+int main() {
+
+	double randd(int,int);
+	double rand_gaosi(int,double);
+	double rand_num;
+	int x[N];
+	int i,j,t;
+	
+	FILE *file, *file2;
+
+	srand((unsigned)time(NULL));
+	
+	file = fopen("/home/wangjie/test/C/xes1/dis/result.txt", "w");
+	file2 = fopen("/home/wangjie/test/C/xes1/dis/result2.txt", "w");
+
+	for (i = 0; i < N; i++) {
+		x[i] = 0;
+	}
+
+	/*统计0-1000间的数的个数*/
+	for ( t = 0; t < 1000000; t++) {
+		rand_num = rand_gaosi(7,0.3);
+		i =  (rand_num +1)*10000 / (20000 / N);
+		x[i]++;
+		fprintf(file2, "%d\t%f\n", i, rand_num);
+	}
+
+	for (i = 0; i < N; i++) {
+		fprintf(file, "%d\t%d\n", i, x[i]);
+	}
+
+	fclose(file);
+
+}
+double rand_gaosi(int preci, double W){
+/* 产生-1 ~ 1, 精度为preci, 半高宽为W的高斯分布的随机数*/
+/* 精度最好远大于统计的间隔数 */
+	double randd();
+	double randx =  randd(0, preci);
+	double randy =  randd(0, preci);
+	double f = exp(-randx * randx / W / W * 4 * log(2));
+	if (randy < f)
+		return rand() % 10000 > 5000 ? randx : -randx;
+	else
+		rand_gaosi(preci, W);
+}
+
+double randd(int n, int m) {
+	return rand()%(int)(pow(10, n+m))/pow(10, m);
+}
+
