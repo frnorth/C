e087fd8692d4127411a4d9d63967cd0e645f1325
frnorth <763211690@qq.com>
2021-03-15 18:42:17 +0800

    a

--- lib_self/dis2_up.c
+++ lib_self/dis2_up.c
@@ -1,3 +1,18 @@
+/*
+
+随机生成(x ,y), 以x为自变量，求对应的高斯(或费米狄拉克分布)函数值f
+如果y < f，则返回x
+如果y > f，则嵌套调用函数
+该方法有很好的适用性，不受限于分布函数的形式
+并且代码较少容易理解
+
+切换注释的内容来改变所需要的分布函数
+
+注意, 精度是int型
+
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
@@ -8,7 +23,9 @@
 
 int main() {
 
+	/* 高斯分布随机数 */
 	double rand_Gaussian(int, double);
+	/* 费米狄拉克分布的随机数 */
 	double rand_Fermi_Dirac(int);
 
 	double rand_num;
@@ -16,6 +33,8 @@ int main() {
 	int x[N];
 	int i,j,t;
 	
+	/* file_numbers 记录所产生的随机数，
+	   file_static 统计随机数的分布 */
 	FILE *file_numbers, *file_static;
 
 	srand((unsigned)time(NULL));
@@ -27,12 +46,17 @@ int main() {
 		x[i] = 0;
 	}
 
-	/*统计0-1间, 精度为10^-7的1000000个数, 的分布, 统计的间隔数(坐标点)为1000*/
+	/* 统计0-1间, 精度为10^-7的1000000个数, 的分布, 统计的间隔数(坐标点)为1000 */
 	for ( t = 0; t < 1000000; t++) {
-		//rand_num = rand_Gaussian(7,0.3);
-		//i = (rand_num +1) * 10000 / (20000 / N);
+
+		/* 用于统计高斯分布的随机数 */
+		/* rand_num = rand_Gaussian(7,0.3);
+		i = (rand_num +1) * 10000 / (20000 / N); */
+
+		/* 用于统计费米狄拉克分布的随机数 */
 		rand_num = rand_Fermi_Dirac(7);
-		i = rand_num * 10000 / (20000 / N);
+		i = rand_num * 10000 / (20000 / N); 
+
 		x[i]++;
 		fprintf(file_numbers, "%d\t%f\n", i, rand_num);
 		if (t % 100000 < 1)
@@ -64,7 +88,7 @@ double rand_Gaussian(int preci, double W){
 		rand_Gaussian(preci, W);
 }
 
-/* 返回-1 ~ 1, 精度为preci, 半高宽为W的高斯分布的随机数*/
+/* 返回 0 ~ 1, 精度为preci, 化学势为0.5, KT = 0.05 的随机数*/
 double rand_Fermi_Dirac(int preci){
 	double randd();
 
